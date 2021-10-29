eaa13fec4f7eb495f3611d7f9a0d80d7e905c24e
synthi <normcs@protonmail.com>
2021-10-29 11:12:18 -0400

    Update meta.ts

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -1,6 +1,6 @@
 export const data = {
     title: 'Couleur Tweak Tips',
     description: `Couleur Tweak Tips is a public and free (as in open-source) repository of very useful knowledge regarding rendering, Windows, and more.`,
-    image: 'https://cdn.discordapp.com/banners/774315187183288411/1d00879f1d8edb85c815b5907cb47f60.jpg?size=240',
+    image: 'https://cdn.discordapp.com/attachments/828538169711656980/903662459234164777/CTT_Desktop.png',
     type: 'website'
 }
