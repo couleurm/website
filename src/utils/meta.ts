38af051d321913c4dc26290217a99d4cf0ebf1e4
Frosty7473 <frostlord7473@gmail.com>
2023-03-18 21:09:03 -0400

    grammar and syntax fixes, as well as general clarifications

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -1,6 +1,6 @@
 export const data = {
     title: 'Couleur Tweak Tips',
-    description: `Couleur Tweak Tips is a public and free (as in open-source) repository of very useful knowledge regarding rendering, Windows, and more.`,
+    description: `Couleur Tweak Tips is a public and open source repository of very useful knowledge regarding video rendering, Windows tweaking, FFmpeg, and more.`,
     image: 'https://cdn.discordapp.com/attachments/828538169711656980/903662459234164777/CTT_Desktop.png',
     type: 'website'
 }
