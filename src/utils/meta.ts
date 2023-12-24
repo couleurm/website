930079faff6591de53857e64c3f83e0ecef8786b
Couleur <82747632+couleurm@users.noreply.github.com>
2023-12-24 11:52:16 +0100

    Update meta.ts

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -1,6 +1,6 @@
 export const data = {
     title: 'Couleur Tweak Tips',
-    description: `Couleur Tweak Tips is a public and free (as in open-source) repository of very useful knowledge regarding rendering, Windows, and more.`,
+    description: `CTT is a website & discord server that documents video production & Windows tuning.`,
     image: 'https://cdn.discordapp.com/attachments/828538169711656980/903662459234164777/CTT_Desktop.png',
     type: 'website'
 }
